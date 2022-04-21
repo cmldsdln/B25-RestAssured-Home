29950851ac5b89bbbf4625fbf6402027de5f302f
Cemil Dasdelen <cmldsdln@gmail.com>
2022-04-21 12:38:33 -0400

    day 3

--- src/test/java/com/cydeo/day3/SpartanTestWithParameters.java
+++ src/test/java/com/cydeo/day3/SpartanTestWithParameters.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -102,6 +105,31 @@ public class SpartanTestWithParameters extends SpartanTestBase {
         assertEquals(200, response.statusCode());
         //verify content type
         assertEquals("application/json", response.contentType());
+        //verify Female inside body
+        assertTrue(response.body().asString().contains("Female"));
+        //verify Janette inside the json body
+        assertTrue(response.body().asString().contains("Janette"));
+
+    }
+
+    @DisplayName("GET request to /api/spartans/search with Query Params (MAP)")
+    @Test
+    public void test4(){
+        //create a map and store query params information
+        Map<String,Object> queryMap = new HashMap<>();
+        queryMap.put("gender","Female");
+        queryMap.put("nameContains","e");
+
+        Response response = given().log().all().
+                accept(ContentType.JSON).
+                queryParams(queryMap)
+                .when().
+                get("/api/spartans/search");
+
+        //verify status code
+        assertEquals(200,response.statusCode());
+        //verify content type
+        assertEquals("application/json",response.contentType());
         //verify Female inside body
         assertTrue(response.body().asString().contains("Female"));
         //verify Janette inside the json body
