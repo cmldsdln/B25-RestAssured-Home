1d5f21e741b0a902b7e4356ce75232b55d688c6d
Cemil Dasdelen <cmldsdln@gmail.com>
2022-04-21 11:36:20 -0400

    TAsks

--- src/test/java/com/cydeo/day2/SpartanNegativeGetRequest.java
+++ src/test/java/com/cydeo/day2/SpartanNegativeGetRequest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+
+
+
+
 public class SpartanNegativeGetRequest {
 
      /*TASK
